6b5e636e215d6f88629ed6723a2697089bf7fe9f
travelingpiano <lihsuanlu@gmail.com>
2020-06-02 23:01:09 -0700

    practice some arrow functions

---
+++ play.js
@@ -0,0 +1,19 @@
+var name = 'Max';
+var age = 29;
+var hasHobbies = true;
+console.log(summarizeUser(name, age, hasHobbies));
+
+function summarizeUser(userName, userAge, userHasHobby) {
+    return 'Name is ' + userName + ', age is ' + userAge + ' and the user has hobbies: ' + userHasHobby;
+}
+
+// arrow functions allow us to use the 'this' keyword
+const summarizeUser1 = (userName, userAge, userHasHobby) => {
+
+};
+
+const add = (a, b) => a + b;
+
+const addOne = a => a + 1;
+
+console.log(addOne(1));
