9877f43e16c823585ae3adc41b0da33853a8de2a
travelingpiano <lihsuanlu@gmail.com>
2020-06-05 09:34:34 -0700

    learned about process.exit

--- first-nodejs-app/app.js
+++ first-nodejs-app/app.js
@@ -2,6 +2,7 @@ const http = require('http'); // look for global module called http
 
 function rqListener(req, res) {
     console.log(req);
+    // process.exit();
 }
 
 const server = http.createServer(rqListener);
