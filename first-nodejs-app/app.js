032411596323eeb3fc09dbe15b57fc18c6974012
travelingpiano <lihsuanlu@gmail.com>
2020-06-07 12:03:19 -0700

    add some basic responses

--- first-nodejs-app/app.js
+++ first-nodejs-app/app.js
@@ -1,13 +1,29 @@
 const http = require('http'); // look for global module called http
+const fs = require('fs');
 
 function rqListener(req, res) {
-    console.log(req.url, req.method, req.headers);
-    // process.exit();
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        res.end();
+        return;
+    }
+    if (url === '/message' && method === 'POST') {
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+    console.log(req);
     res.setHeader('Content-Type', 'text/html');
     // write allows us to write some data to the response
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write(`<body><h1>Hello from my Node.js Server! ${req.body}</h1></body>`);
     res.write('</html>');
     res.end();
 }
