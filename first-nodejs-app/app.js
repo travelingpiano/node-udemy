f004aabcbf10535db2ad95f361a5c3eedda35aed
travelingpiano <lihsuanlu@gmail.com>
2020-06-07 11:44:21 -0700

    basic req, res read write

--- first-nodejs-app/app.js
+++ first-nodejs-app/app.js
@@ -1,8 +1,15 @@
 const http = require('http'); // look for global module called http
 
 function rqListener(req, res) {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
     // process.exit();
+    res.setHeader('Content-Type', 'text/html');
+    // write allows us to write some data to the response
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
 }
 
 const server = http.createServer(rqListener);
