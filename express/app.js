049dd2be0bc9de594da07526fe468e3dcc2d382f
travelingpiano <lihsuanlu@gmail.com>
2020-08-16 11:21:48 -0700

    added body parser

--- express/app.js
+++ express/app.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 // executed for every incoming request
 // should either call next or return response
 app.use('/add-product', (req, res, next) => {
     console.log('Adding some products');
-    res.send('<h1>Adding Products</h1>');
+    res.send('<form action="/product" method="post"><input text="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+// the next is technically optional
+app.use('/product', (req, res) => {
+    console.log(req.body['title']);
+    res.redirect('/');
 });
 
 app.use((req, res, next) => {
