e0915d64583265c171c4d6485b574a0b922fb6f9
travelingpiano <lihsuanlu@gmail.com>
2020-08-16 12:15:11 -0700

    add 404 and also path filtering

--- express/app.js
+++ express/app.js
@@ -5,10 +5,13 @@ const userRouter = require('./routes/shop');
 
 const app = express();
 
-app.use(adminRouter);
+app.use(bodyParser.urlencoded({extended: false}));
 
+app.use('/admin', adminRouter);
 app.use(userRouter);
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+})
 
 app.listen(3000);
--- express/routes/shop.js
+++ express/routes/shop.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-router.use((req, res, next) => {
+router.get((req, res, next) => {
     console.log('In another middleware');
     res.send('<h1>Hello from Express</h1>');
 });
